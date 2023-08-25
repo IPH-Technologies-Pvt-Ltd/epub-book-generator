2abf6710afe7db9552c09f3d74e5c38d5b49a9e7
Pavan Kumar Singh <133772531+pavan-singh-iphtech@users.noreply.github.com>
2023-08-25 15:04:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import EPUBGenerator from './pages/MyEpub';
+function App() {
+  return (
+    <div className="App">
+      <EPUBGenerator />
+    </div>
+  );
+}
+
+export default App;
